a9d930fb4a86c18217a6f61a0a204842a649b089
Lakshmi Prasanna <166805623+LakshmiPrasanna2404@users.noreply.github.com>
2024-12-13 12:20:04 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,9 @@
+
+t=int(input("Enter your salary:"))
+a=int(input("Enter the shopping bill number1:"))
+b=int(input("Enter the shopping bill number2:"))
+c=int(input("Enter the shopping bill number3:"))
+l=a+b+c
+print(f"Total salary spent on shopping:{l}")
+p=l*100/t
+print(f"The percentage of the salary spent on shopping is:{p}")
